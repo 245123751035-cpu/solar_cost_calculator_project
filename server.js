0a7e3e5343cd724b3e5193e9332f2d14afa5e0f1
Kshitij <245123751035@mvsrec.edu.in>
2026-07-16 13:59:59 +0530

    Add Vercel config and update server for deployment

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname)));
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/first.html');
+  res.sendFile(path.join(__dirname, 'first.html'));
 });
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
+
+module.exports = app;
