c372a586b9866d1a87eb786bca60904c45cee494
Kshitij <245123751035@mvsrec.edu.in>
2026-07-16 13:53:31 +0530

    Initial commit: Solar Cost Calculator Project

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/first.html');
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
